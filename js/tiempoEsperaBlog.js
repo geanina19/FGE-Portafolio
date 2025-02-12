5d4545c4cb9ded9f4c26e52b309c584ddf9310fb
Geanina <geany849@gmail.com>
2025-02-12 13:40:38 +0100

    mejoras proyectos

--- js/tiempoEsperaBlog.js
+++ js/tiempoEsperaBlog.js
@@ -2,13 +2,22 @@ function showLoader(event) {
     // Prevenir que el comportamiento predeterminado de la acción del clic ocurra
     event.preventDefault();
 
-    // Mostrar el overlay con los spinners
-    const loaderOverlay = document.getElementById('loaderOverlay');
-    loaderOverlay.style.display = 'flex';
+    // Verificar si ya se ha mostrado el loader en esta sesión
+    if (!sessionStorage.getItem('loaderShown')) {
+        // Mostrar el overlay con los spinners
+        const loaderOverlay = document.getElementById('loaderOverlay');
+        loaderOverlay.style.display = 'flex';
 
-    // Esperar 3 segundos (3000 ms) antes de proceder con el redireccionamiento
-    setTimeout(function () {
-        // Redirigir a la URL deseada directamente
-        window.location.href = 'https://computacioncuanticayeany.netlify.app/';  // URL específica para redirigir
-    }, 400); 
+        // Marcar que el loader ya ha sido mostrado en esta sesión
+        sessionStorage.setItem('loaderShown', 'true');
+
+        // Esperar 3 segundos (3000 ms) antes de proceder con el redireccionamiento
+        setTimeout(function () {
+            // Redirigir a la URL deseada directamente
+            window.location.href = 'https://computacioncuanticayeany.netlify.app/';  // URL específica para redirigir
+        }, 400);
+    } else {
+        // Si el loader ya fue mostrado, solo redirigir
+        window.location.href = 'https://computacioncuanticayeany.netlify.app/';
+    }
 }
